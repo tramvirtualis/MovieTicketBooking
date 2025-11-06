eb5a2d7278211297e96a1849a895b2fd391690df
ThienNhan-Vermillion <phuongthiennhan04@gmail.com>
2025-11-06 17:31:29 +0700

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-200">
+      <h1 className="text-5xl font-bold text-blue-600">
+        TailwindCSS v3 is working perfectly 🚀
+      </h1>
+    </div>
+  );
+}
